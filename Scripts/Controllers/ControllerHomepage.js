9220c26fd2e07042bddf65c94140a824fad0a6d7
Lonatri <gyda-94@hotmail.com>
2025-10-20 13:43:02 +0200

    AvgQuality

--- Scripts/Controllers/ControllerHomepage.js
+++ Scripts/Controllers/ControllerHomepage.js
@@ -1,7 +1,18 @@
 //average is the sum of the numbers divided on the amount of numbers
-function averageQuality(){
-    
+//functions to find average of sleep quality, sleep length and condition during the day
 
+const currentUserLogs = model.data.registeredLogs.find(logs => logs.userID === model.app.currentUser);
+console.log(currentUserLogs)
+function averageQuality(){
+    let sum = 0;
+    for(let log of currentUserLogs.lists){
+        console.log(log.timeSleptNum)
+        sum += log.timeSleptNum;
+        console.log('total sleep time:', sum)
+    }
+    let avg = sum/currentUserLogs.lists.length;
+    console.log('average sleep time:', avg);
+    return avg;
 }
 function averageLength(){
 
@@ -9,3 +20,11 @@ function averageLength(){
 function averageCondition(){
 
 }
+
+// // Hvis registeredLogs har flere instanser av userId: 1, så kommer den til å bare finne den første
+// // Finner objektet og loggene til bruker som er "innlogget" med currentUser
+// const currentUserLogs = model.data.registeredLogs.find(logs => logs.userID === model.app.currentUser)
+// console.log(currentUserLogs)
+// for(let log of currentUserLogs.lists){
+//     console.log(log)
+// }
