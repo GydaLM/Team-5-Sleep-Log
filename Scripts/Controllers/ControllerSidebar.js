c31545fe30f690b017a6dde9bba26e3f0e795e91
Lene <Lene-tl@hotmail.com>
2025-10-20 12:58:50 +0200

    Update ControllerSidebar.js

---
+++ Scripts/Controllers/ControllerSidebar.js
@@ -0,0 +1,24 @@
+//-----------------Lene
+
+//function sidebarToggle() {              // funskjon som åpner og lukker Sidebar-en
+// sidebarOpen = !sidebarOpen;             // Toggler mellom False og true verdi
+//if (sidebarOpen) {
+//console.log("sidebar-en er åpen")
+//} else {
+//console.log("sidebar-en er stengt")
+//}
+//}
+
+//--------------------------------onclick functions for sidebar buttons
+function changeViewToHomepage() {
+    currentpage = 'homepage';
+}
+function changeViewToLogList() {
+    currentpage = 'loglist';
+}
+//function changeViewToStatistics() {currentpage = 'statistics';}
+function changeViewToLogIn() {
+    //need some log out funtion?
+    //current user = ''; ?
+    currentpage = 'logIn';
+}
