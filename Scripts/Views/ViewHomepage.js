3c363bafd55e0f631978dad6e43ef57c8fe0d7d0
mstorm04 <mstorm042004@gmail.com>
2025-10-21 14:11:46 +0200

    Started writing ViewHomepage og StyleHomepage

--- Scripts/Views/ViewHomepage.js
+++ Scripts/Views/ViewHomepage.js
@@ -2,6 +2,13 @@ function updateHomepage(){
     return /*HTML*/ `
     <div>${SideBar()}</div>
     <div>This is the Homepage!</div>
-    
+    <div id='mainHomepageDiv'>
+        <div>Gjennomsnitt de siste syv loggene</div>
+        <div id='statsHomepage'>
+            <div class='avgDisplayHomepage'>Søvn Kvalitet <br> ${averageQuality()}/5</div>
+            <div class='avgDisplayHomepage'>Søvn lengde <br>${averageLength().hours}t ${averageLength().minutes}min</div>
+            <div class='avgDisplayHomepage'>Dagsform <br> ${averageCondition()}/5</div>
+        </div>
+    </div>
 `
 }
