c5b9aec189164ef548eb9ed1ad12625054c0f021
Lene <Lene-tl@hotmail.com>
2025-10-22 14:06:20 +0200

    Update ViewLogInput.js

--- Scripts/Views/ViewLogInput.js
+++ Scripts/Views/ViewLogInput.js
@@ -14,23 +14,23 @@ function updateLogInput(){
     </div>
 <br>
     <div class="inputWrap">
-        <label for="awoken">Våknet:</label><input id="awoken" type="time">
-        <label for="gotUp">Sto opp:</label><input id="gotUp" type="time">
+        <label for="awoken">Våknet:</label><input id="awoken" type="time" oninput="saveAwoken(this.value)">
+        <label for="gotUp">Sto opp:</label><input id="gotUp" type="time" oninput="saveGotUp(this.value)">
     </div>
 <br>
     <div class="inputWrap">
-        <label for="timeDisturbed">Våknet i natt:</label><input id="timeDisturbed" type="number">
-        <label for="timeawaken">Tid våken:</label><input id="timeawaken" type="number">
-        <label for="napTime">Lur-tid:</label><input id="napTime" type="number">
+        <label for="timeDisturbed">Våknet i natt:</label><input id="timeDisturbed" type="number" oninput="saveSleepDisturbed(this.value)">
+        <label for="timeawaken">Tid våken:</label><input id="timeawaken" type="number" oninput="saveTimeDisturbed(this.value)">
+        <label for="napTime">Lur-tid:</label><input id="napTime" type="number" oninput="saveNapTime(this.value)">
     </div>
 <br>
     <div class="inputWrap">
-        <label for="quality">Kvalitet</label><input id="quality">
-        <label for="condition">Dagsform</label><input id="condition">
+        <label for="quality">Kvalitet</label><input id="quality" oninput="saveQuality(this.value)">
+        <label for="condition">Dagsform</label><input id="condition" oninput="saveCondition(this.value)">
     </div>
 <br>
     <div class="notesWrap">
-        <label for="notes">Notater:</label><input id="notes">
+        <label for="notes">Notater:</label><input id="notes" oninput="saveNotes(this.value)">
     </div>
 <br>
     <button>Lagre</button>
