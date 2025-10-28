e3a93e27bc829af04cdb28c6fba2dcd37008f613
Lonatri <gyda-94@hotmail.com>
2025-10-28 13:24:37 +0100

    EditLog updated

--- Scripts/Controllers/ControllerEditLog.js
+++ Scripts/Controllers/ControllerEditLog.js
@@ -1,19 +1,13 @@
 //---Gyda
 
-// function saveEditedLog(){
-//     const currentUserLogs = model.data.registeredLogs.findIndex(logs => logs.userID === model.app.currentUser);
-//     let logIndex = model.data.registeredLogs[currentUserLogs].lists.findIndex(log => log.logID === Number(model.viewstate.selectedLog));
-//     model.data.registeredLogs[currentUserLogs].lists[logIndex] = {
-//             bedtime: model.viewstate.editLog.bedtime,
-//             attemptedSleep: model.viewstate.editLog.attemptedSleep,
-//             fellAsleep: model.viewstate.editLog.fellAsleep,
-//             awoken: model.viewstate.editLog.awoken,
-//             gotUp: model.viewstate.editLog.gotUp,
-//             sleepDisturbed: model.viewstate.editLog.sleepDisturbed,
-//             timeDisturbed: model.viewstate.editLog.timeDisturbed,
-//             napTime: model.viewstate.editLog.napTime,
-//             quality: model.viewstate.editLog.quality,
-//             condition: model.viewstate.editLog.condition,
-//             notes: model.viewstate.editLog.notes,
-//         }
-// }
+function saveEditedLog(){
+    const currentUserLogs = model.data.registeredLogs.find(logs => logs.userID === model.app.currentUser);
+    const logIndex = currentUserLogs.lists.findIndex(log => log.logID === Number(model.viewstate.selectedLog));
+
+    currentUserLogs.lists[logIndex] = {
+        ...currentUserLogs.lists[logIndex],
+        ...model.viewstate.editLog,
+    };
+    model.app.currentPage = 'savedLog';
+    changeView();
+}
