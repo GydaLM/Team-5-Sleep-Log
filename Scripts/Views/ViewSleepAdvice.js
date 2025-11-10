fbb15f2670df40c38092da18b483329e76dbc69d
Lene <Lene-tl@hotmail.com>
2025-11-10 14:17:28 +0100

    SleepAdvice

--- Scripts/Views/ViewSleepAdvice.js
+++ Scripts/Views/ViewSleepAdvice.js
@@ -1,6 +1,19 @@
+//-----------------Lene, Remy og Gyda! ♥ ♥ ♥ 
+
 function updateSleepAdvice(){
     return /*HTML*/`
-        <h1>Vansker med å sove?</h1>
+
+        <h1 class="Overskrift">Vansker med å sove?</h1>
+        <p class="informationBox"> De fleste har problemer med å sove innimellom. <br> 
+            Det er ikke farlig å ha en periode med dårlig søvn, men om du ofte har søvnproblemer som gjør at du fungerer dårlig i hverdagen, så kan det være nyttig å gjøre noen endringer. <br> 
+            <br>
+            På denne siden finner du råd som kan hjelpe til bedre søvn.</p>
+        <div class="informationBox"> 
+        <h2>Stå opp til samme tid</h2>
+        <p>Legg deg og stå opp til omtrent samme tid hver dag, også i helger.<br>
+        Et fast søvnmønster hjelper kroppen til å regulere døgnrytmen, slik at man blir trøtt til samme tid.<br>
+        Da kan man ungå å ligge i sengen of ikke få sovet, fordi man sov for lenge</p>
+        </div>
 
 
 `
