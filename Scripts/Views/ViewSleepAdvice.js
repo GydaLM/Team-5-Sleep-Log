e96c6f59d2be5e813ee9f94445c70248e5b0fd39
Lene <Lene-tl@hotmail.com>
2025-11-11 11:03:44 +0100

    SleepAdvice

--- Scripts/Views/ViewSleepAdvice.js
+++ Scripts/Views/ViewSleepAdvice.js
@@ -4,15 +4,31 @@ function updateSleepAdvice(){
     return /*HTML*/`
 
         <h1 class="Overskrift">Vansker med å sove?</h1>
-        <p class="informationBox"> De fleste har problemer med å sove innimellom. <br> 
+        <p class="SleepAdviceInformationBox1"> De fleste har problemer med å sove innimellom. <br> 
             Det er ikke farlig å ha en periode med dårlig søvn, men om du ofte har søvnproblemer som gjør at du fungerer dårlig i hverdagen, så kan det være nyttig å gjøre noen endringer. <br> 
             <br>
             På denne siden finner du råd som kan hjelpe til bedre søvn.</p>
-        <div class="informationBox"> 
+        <div class="SleepAdviceInformationBox2"> 
         <h2>Stå opp til samme tid</h2>
         <p>Legg deg og stå opp til omtrent samme tid hver dag, også i helger.<br>
         Et fast søvnmønster hjelper kroppen til å regulere døgnrytmen, slik at man blir trøtt til samme tid.<br>
         Da kan man ungå å ligge i sengen of ikke få sovet, fordi man sov for lenge</p>
+        </div>
+        <div class="SleepAdviceInformationBox1"> 
+        <h2>Unngå å sove på dagtid</h2>
+        <p>Søvnbehovet bygger seg opp i løpet av dagen og er høyest på kvelden. Hvis du sover i løpet av dagen blir dette behovet mindre noe som kan påvirke søvnen. Trenger man en lur i løpet av dagen er det best å holde den under 20 min.</p>
+        </div>
+        <div class="SleepAdviceInformationBox2"> 
+        <h2>Ikke ligg våken i sengen</h2>
+        <p>Stå opp og gjør noe annet:
+            Unngå å ligge våken i senga. Hvis man ikke klarer å sove eller våkner i løpet av natten, kan det være lurt å stå opp litt. Gjør noe rolig i et annet rom en liten stund, for eksempel les en bok i svakt lys, drikk litt vann eller lignende.</p>
+        </div>
+        <div class="SleepAdviceInformationBox1"> 
+        <h2>Mer tid foran skjerm gir mindre tid med søvn</h2>
+        <p>Bruk av elektorniske skjermer både dag og kveld påvirker søvnen negativt.<br>
+           Jo større bruken av elekroniske skjermer er i timene før leggetid,<br>
+           jo mer øker risikoen for at søvntiden forkortes,og at leggetiden forskyves inn i senere nattetimer.<br>
+           Mer tid foran skjerm gir mindre tid med søvn</p>
         </div>
 
 
