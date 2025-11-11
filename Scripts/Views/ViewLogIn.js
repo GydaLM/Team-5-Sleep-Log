312bcaae6b5135b86590727d3fcdbaafb693a563
Lonatri <gyda-94@hotmail.com>
2025-11-11 11:21:42 +0100

    LogIn Btn

--- Scripts/Views/ViewLogIn.js
+++ Scripts/Views/ViewLogIn.js
@@ -10,7 +10,7 @@ function updateLogIn(){
     <div>
     <input placeholder="Brukernavn" oninput="model.viewstate.logIn.name = this.value">
     <input type="password" id="passwordField" placeholder="Passord" oninput="model.viewstate.logIn.password = this.value">
-    <button onclick="logIn()">Log in</button>
+    <button class="logInBtn" onclick="logIn()">Log in</button>
 
 
     </div>
